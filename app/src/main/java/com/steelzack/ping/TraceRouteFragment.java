77deaf942c11a16f1e9d26c004e09fafee914ab4
Joao Esperancinha <jofisaes@hotmail.com>
2016-01-10 23:17:03 +0100

    SZ9 All IPs complete

--- app/src/main/java/com/steelzack/ping/TraceRouteFragment.java
+++ app/src/main/java/com/steelzack/ping/TraceRouteFragment.java
@@ -1,22 +1,63 @@
 package com.steelzack.ping;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.steelzack.ping.util.SystemUiHider;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TraceRouteFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TraceRouteFragment#newInstance} factory method to
- * create an instance of this fragment.
+ *
  */
 public class TraceRouteFragment extends android.support.v4.app.Fragment {
+
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * If set, will toggle the system UI visibility upon interaction. Otherwise,
+     * will show the system UI visibility upon interaction.
+     */
+    private static final boolean TOGGLE_ON_CLICK = true;
+
+    /**
+     * The flags to pass to {@link SystemUiHider#getInstance}.
+     */
+    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+
+    /**
+     * The instance of the {@link SystemUiHider} for this activity.
+     */
+    private SystemUiHider mSystemUiHider;
+
+    private EditText editText = null;
+
+    private Button btnCalculate = null;
+
+    private TextView textView = null;
+
+    private TextView textResult = null;
+
+    private TextView textComment = null;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -25,8 +66,6 @@ public class TraceRouteFragment extends android.support.v4.app.Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public TraceRouteFragment() {
         // Required empty public constructor
@@ -38,7 +77,7 @@ public class TraceRouteFragment extends android.support.v4.app.Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TraceRouteFragment.
+     * @return A new instance of fragment PingFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static TraceRouteFragment newInstance(String param1, String param2) {
@@ -52,45 +91,61 @@ public class TraceRouteFragment extends android.support.v4.app.Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+        View mainView = inflater.inflate(R.layout.fragment_trace_route_layout, container, false);
+        final View contentView = mainView.findViewById(R.id.fullscreen_content);
+
+
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+        editText = (EditText) mainView.findViewById(R.id.editDNS);
+        btnCalculate = (Button) mainView.findViewById(R.id.btnCalculate);
+        textView = (TextView) mainView.findViewById(R.id.textView);
+        textResult = (TextView) mainView.findViewById(R.id.txtResult);
+        textComment = (TextView) mainView.findViewById(R.id.textComment);
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                long nowStamp = System.currentTimeMillis();
+                String ipAddress = editText.getText().toString();
+
+                try {
+                    InetAddress[] inets = InetAddress.getAllByName(ipAddress);
+                    final StringBuffer sb = new StringBuffer();
+                    sb.append(String.format("Hostname:%s",ipAddress));
+                    for(InetAddress inet : inets){
+                        sb.append(String.format("\n%s", inet.getHostAddress()));
+                    }
+
+                    textResult.setText(sb.toString());
+
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                    textResult.setText(String.format("Host not found!\n%s", e.getMessage()));
+                }
+                long endStamp = System.currentTimeMillis();
+                textComment.setText(String.format("Ping lasted %d miliseconds", (endStamp - nowStamp)));
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trace_route_layout, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        return mainView;
     }
 }
