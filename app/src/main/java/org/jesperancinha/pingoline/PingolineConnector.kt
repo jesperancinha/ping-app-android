aa1c79f32769166c6e494234acb03dc0d061a571
Joao Esperancinha <jofisaes@gmail.com>
2023-08-20 12:45:05 +0200

    Upgrades images and minimizes side-effects

---
+++ app/src/main/java/org/jesperancinha/pingoline/PingolineConnector.kt
@@ -0,0 +1,37 @@
+package org.jesperancinha.pingoline
+
+import java.net.InetAddress
+
+class LogData(
+    val result: String,
+    val comment: String = ""
+)
+
+object PingolineConnector {
+
+    fun test(ipAddress: String): Result<LogData> {
+        val nowStamp = System.currentTimeMillis()
+        return kotlin.runCatching {
+            val inet = InetAddress.getByName(ipAddress)
+            val textResult =
+                String.format(
+                    "Adress:%s\nHostname:%s",
+                    inet.hostAddress,
+                    inet.hostName
+                )
+            val endStamp = System.currentTimeMillis()
+            val comment =
+                String.format(
+                    "Ping lasted %d miliseconds",
+                    endStamp - nowStamp
+                )
+            LogData(
+                result = textResult,
+                comment = comment
+            )
+        }.onFailure {
+            val textResult = String.format("Host not found!\n%s", it.message)
+            LogData(result = textResult)
+        }
+    }
+}
